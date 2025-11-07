8faf5ecf1043b830180f8ade7cf9911b44e2b5d5
raghulprabu <raghulitsme23@gmail.com>
2025-11-07 23:17:49 +0530

    first commit

---
+++ Function Object Task/Function/function_filter.js
@@ -0,0 +1,4 @@
+const fruits =["apple", "orange", "Mango" , "graps", "Jackfruits"]
+console.log(fruits)
+const largest = fruits.filter(fruits => fruits.length>5);
+console.log(largest)
---
+++ Function Object Task/Function/function_map.js
@@ -0,0 +1,3 @@
+ let number = [ 1,2,3,4,5];
+ let double = number.map(number=> number*2)
+ console.log(double)
